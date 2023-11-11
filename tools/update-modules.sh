6913b5a737d7387ed27dc5ed908637c2e8ab3eb2
sugarcanehyena <sugarcanehyena@protonmail.com>
2023-11-11 14:59:31 +0000

    Make `OFAC Filter List` daily/weekly auto-update
    x

--- tools/update-modules.sh
+++ tools/update-modules.sh
@@ -17,6 +17,37 @@ assets=(
     ['src/blacklists/tracking-filters/nomalware.txt']='https://curbengh.github.io/malware-filter/tracking-filter.txt'
     ['src/blacklists/urlhaus-filters/hosts.txt']='https://curbengh.github.io/malware-filter/vn-badsite-filter-hosts.txt'
     ['src/blacklists/urlhaus-filters/nomalware.txt']='https://curbengh.github.io/malware-filter/vn-badsite-filter.txt'
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_ARB.json]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_ARB.json
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_ARB.txt]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_ARB.txt
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_BCH.json]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_BCH.json
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_BCH.txt]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_BCH.txt
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_BSC.json]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_BSC.json
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_BSC.txt]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_BSC.txt
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_BSV.json]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_BSV.json
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_BSV.txt]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_BSV.txt
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_BTG.json]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_BTG.json
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_BTG.txt]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_BTG.txt
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_DASH.json]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_DASH.json
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_DASH.txt]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_DASH.txt
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_ETC.json]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_ETC.json
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_ETC.txt]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_ETC.txt
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_ETH.json]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_ETH.json
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_ETH.txt]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_ETH.txt
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_LTC.json]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_LTC.json
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_LTC.txt]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_LTC.txt
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_USDT.json]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_USDT.json
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_USDT.txt]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_USDT.txt
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_XBT.csv]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_XBT.csv
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_XBT.json]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_XBT.json
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_XBT.txt]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_XBT.txt
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_XMR.json]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_XMR.json
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_XMR.txt]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_XMR.txt
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_XRP.json]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_XRP.json
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_XRP.txt]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_XRP.txt
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_XVG.json]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_XVG.json
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_XVG.txt]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_XVG.txt
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_ZEC.json]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_ZEC.json
+    ['src/ofac-sanctioned-digital-currency-addresses/sanctioned_addresses_ZEC.txt]='https://raw.githubusercontent.com/0xB10C/ofac-sanctioned-digital-currency-addresses/lists/sanctioned_addresses_ZEC.txt
 )
 
 for i in "${!assets[@]}"; do
